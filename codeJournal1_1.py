877df83553ee58c0bb376f345f167abcf55062d5
rkatare17 <rkatare@ucsc.edu>
2024-10-16 16:30:51 -0700

    Session 1 prompt for Code Journal 1

---
+++ codeJournal1_1.py
@@ -0,0 +1,18 @@
+def PrintName():
+    print("\n\n\nHi, I'm Ridhima Katare!")
+
+def PrintPronouns():
+    print("My pronouns are she/her.")
+
+def PrintSentences():
+    print("My favorite movie is Ferris Bueller's Day Off.")
+    print("My favorite food is a type of Indian food called Pav Bhaji, it's this potato vegetable stew that you eat with bread roles.\n\n\n")
+
+
+def main():
+    PrintName()
+    PrintPronouns()
+    PrintSentences()
+
+if __name__=="__main__":
+    main()
